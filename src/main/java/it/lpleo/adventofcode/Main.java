37a5320431486fbcf45adab53ad2c0f9bbe02249
Leonardo Piccoli <lpleo@hotmail.it>
2020-11-22 19:08:09 +0100

    added problem selector by java arg

--- src/main/java/it/lpleo/adventofcode/Main.java
+++ src/main/java/it/lpleo/adventofcode/Main.java
@@ -3,11 +3,28 @@ package it.lpleo.adventofcode;
 import it.lpleo.adventofcode.domain.PuzzleSolver;
 import it.lpleo.adventofcode.y2019.PuzzleSolvers;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-        List<PuzzleSolver> puzzleSolvers = PuzzleSolvers.instantiate2019PuzzleSolvers();
-        PuzzleSolvers.solvePuzzles(puzzleSolvers);
+
+  public static void main(String[] args) {
+    int specificPuzzle = getNumber(args[0]);
+    List<PuzzleSolver> puzzleSolvers = PuzzleSolvers.instantiate2019PuzzleSolvers();
+    if (specificPuzzle > 0 && puzzleSolvers.size() > specificPuzzle) {
+      puzzleSolvers = Collections.singletonList(puzzleSolvers.get(specificPuzzle));
     }
+    PuzzleSolvers.solvePuzzles(puzzleSolvers);
+  }
+
+  private static int getNumber(String candidateNumber) {
+    if (candidateNumber != null && candidateNumber.length() > 0) {
+      try {
+        return (Integer.parseInt(candidateNumber) - 1);
+      } catch (Exception e) {
+        return -1;
+      }
+    }
+    return -1;
+  }
 }
